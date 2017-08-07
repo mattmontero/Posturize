3e9b2dc27f33a1519b7e878a4c25693d9c79dab5
Matthew Montero <matthewmontero@Matthews-MacBook-Pro.local>
2017-08-06 17:00:02 -0700

    Created ConnectThread, starting Bluetooth branch..

--- app/src/main/java/edu/sjsu/posturize/posturize/bluetooth/ConnectThread.java
+++ app/src/main/java/edu/sjsu/posturize/posturize/bluetooth/ConnectThread.java
@@ -1,8 +1,29 @@
 package edu.sjsu.posturize.posturize.bluetooth;
 
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
 /**
  * Created by matthewmontero on 8/6/17.
  */
 
 public class ConnectThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    public ConnectThread(BluetoothDevice device) {
+        BluetoothSocket tmp = null;
+        try{
+            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            Log.d("UUID", MY_UUID.toString());
+            Log.d("socket", tmp.toString());
+        } catch(IOException e){
+            Log.d("tmp Socket", e.toString());
+        }
+        mmSocket = tmp;
+    }
 }
