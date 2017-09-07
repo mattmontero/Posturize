052fb20d287d697a74725c0674528d2c6059aa0e
mattmontero <matthewrmontero@gmail.com>
2017-09-07 11:17:24 -0700

    Update BluetoothMiniPro.ino
    Clean up

--- hardware/BluetoothMiniPro.ino
+++ hardware/BluetoothMiniPro.ino
@@ -3,14 +3,6 @@
 #define rxPin 0 //define rx data pin
 #define txPin 1 //define tx data pin
 
-// Declare the pins for the Button and the LED
-const short buttonPin = 12;
-const short RED_LED = 13;
-const short GREEN_LED = 6;
-const short WRITE_LED = 4;
-short counter = 0;
-char commandChar;
-
 //create instance of SoftwareSerial
 SoftwareSerial bluetooth(rxPin, txPin);
 
@@ -18,9 +10,6 @@ SoftwareSerial bluetooth(rxPin, txPin);
 void setup() {
   // Define pin #12 as input and activate the internal pull-up resistor
   pinMode(buttonPin, INPUT_PULLUP);
-  // Define pin #13 as output, for the LED
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
 
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
@@ -35,66 +24,11 @@ void setup() {
 void loop() {
   // Read the value of the input. It can either be 1 or 0
   int buttonValue = digitalRead(buttonPin);
-  if (buttonValue == LOW) { //Button Pressed
-
-    // If button pushed, turn LED on
-    digitalWrite(GREEN_LED, HIGH);
-    digitalWrite(RED_LED, LOW);
-
-    if (bluetooth.available()) {
+  if (buttonValue == LOW && bluetooth.available()) { //Button Pressed
       Serial.print("Hardware Pressed");
       Serial.print("#");
-      //bluetooth.print("Hardware Pressed#");
       delay(3000);
-    }
-
-  } else { //Button not pressed.
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(RED_LED, HIGH);
-    //Turn off LED
-    //byte index = 0;
-
-
-
-    /*
-      if (bluetooth.available() > 0) {
-      char inChar;
-      if (index < 63) {
-        inChar = bluetooth.read(); //Read a char
-        inData[index] = inChar; //Store char in inData string
-        index++;
-        if (index == 63) {
-          inData[index] = '#'; //Null terminate the string
-        }
-      }
-      if (index < 63) {
-        inData[index] = '#';
-      }
-      for (int i = 0; i < 64; i++){
-        Serial.print(inData[i]);
-      }
-      Serial.print("#");
-      delay(2000);
-
-      for (byte i = 0; i < 2; i++) {
-        switch (inChar) {
-          case '*':
-            //Serial.print("You sent a star!\n#");
-            break;
-          case 'm':
-            // Serial.print("You sent an m!\n#");
-            break;
-          default:
-            Serial.print("You sent a ... ");
-            Serial.print(inData[i]);
-            Serial.print("\n#");
-            delay(1000);
-
-            break;
-        }
-      }
-      }*/
-  }
+  } 
     delay(1000);
     typedef union {
       float floatingPoint;
