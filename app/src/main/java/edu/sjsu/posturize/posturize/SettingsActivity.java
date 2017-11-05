c2b53ef0559708639611ca41f5988aed59238519
mattmontero <matthewrmontero@gmail.com>
2017-11-05 14:32:15 -0800

    Activity cleanup. Add PostureManager/localdb content to settings page

--- app/src/main/java/edu/sjsu/posturize/posturize/SettingsActivity.java
+++ app/src/main/java/edu/sjsu/posturize/posturize/SettingsActivity.java
@@ -1,6 +1,7 @@
 package edu.sjsu.posturize.posturize;
 
 import edu.sjsu.posturize.posturize.bluetooth.*;
+import edu.sjsu.posturize.posturize.data.localdb.PostureManager;
 import edu.sjsu.posturize.posturize.users.PosturizeUserInfo;
 
 import android.bluetooth.BluetoothAdapter;
@@ -8,23 +9,15 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity
@@ -32,22 +25,9 @@ public class SettingsActivity extends AppCompatActivity
 
     private  BluetoothConnection mBluetoothConnection;
     private SharedPreferences sharedPreferences;
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
     private TextView mTextView;
     private Button mConnectButton;
+    private PostureManager tempPm;
 
 
     @Override
@@ -55,37 +35,22 @@ public class SettingsActivity extends AppCompatActivity
         Log.d("MainActivity", "onCreate: Starting");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        this.setTitle(getString(R.string.signed_in_greeting, PosturizeUserInfo.getInstance().getFirstName()));
-        //setupSharedPreferences();
         setViewsAndListeners();
 
         mBluetoothConnection = BluetoothConnection.getInstance();
         mBluetoothConnection.setTextView(mTextView);
 
+        tempPm = new PostureManager(this.getApplicationContext());
+
         updateUI();
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        mTextView = (TextView) findViewById(R.id.numberViewer);
-
-        Log.d("onCreate","Done");
     }
 
     private void updateUI(){
         if(mBluetoothConnection.isConnected()){
-            ((Button) findViewById(R.id.calibrateButton)).setEnabled(true);
+            findViewById(R.id.calibrateButton).setEnabled(true);
             mConnectButton.setText("Disconnect");
         } else {
-            ((Button) findViewById(R.id.calibrateButton)).setEnabled(false);
+            findViewById(R.id.calibrateButton).setEnabled(false);
             mConnectButton.setText("Connect");
         }
     }
@@ -130,65 +95,28 @@ public class SettingsActivity extends AppCompatActivity
             updateUI();
             return false;
         }
-
         Log.d(BLUETOOTH, mDevice.getName());
         Log.d(BLUETOOTH, mDevice.toString());
 
         //4. Create the connection thread
-
-        //What happens if connectThread fails?
         mBluetoothConnection.connectThread(mDevice);
         Log.d("ConnectThread", "created");
-        //What happens if connectThread does not start?
         mBluetoothConnection.startConnectThread();
-        /*
-         if(mBluetoothConnection.startConnectThread()){
-            mConnectButton.setText("Disconnect");
-            updateUI();
-            return true;
-         } else {
-            Log.d("ConnectThread", "Something went wrong..");
-            return false
-         */
         Log.d("ConnectThread", "Running...");
-        mConnectButton.setText("Disconnect");
+        mConnectButton.setText("Disconnect"); //TODO: move to onConnected method when Bt replies with confirmtion
         updateUI();
         return true;
-
     }
 
     private void setViewsAndListeners(){
+        this.setTitle(getString(R.string.signed_in_greeting, PosturizeUserInfo.getInstance().getFirstName()));
         mTextView = (TextView)findViewById(R.id.numberViewer);
         mConnectButton = (Button) findViewById(R.id.connectButton);
-        //((Button) findViewById(R.id.frontporch_signInButton)).setOnClickListener(this);
         mConnectButton.setOnClickListener(this);
-        //((Button) findViewById(R.id.signoutButton)).setOnClickListener(this);
-        ((Button) findViewById(R.id.calibrateButton)).setOnClickListener(this);
-        ((Button) findViewById(R.id.refreshButton)).setOnClickListener(this);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        findViewById(R.id.calibrateButton).setOnClickListener(this);
+        findViewById(R.id.add_record).setOnClickListener(this);
+        findViewById(R.id.delete_records).setOnClickListener(this);
+        findViewById(R.id.display_records).setOnClickListener(this);
     }
 
     private void calibrate(){
@@ -206,15 +134,11 @@ public class SettingsActivity extends AppCompatActivity
             mTextView.setText("Connecting...");
             if(connectBLE()){
                 mTextView.setText("Connected!");
-                ((Button)findViewById(R.id.connectButton)).setText("Disconnect");
+                ((Button)findViewById(R.id.connectButton)).setText("Disconnect");//TODO: Move to onConnect method when Bt replies with confrimation
             } else {
                 mTextView.setText("Something bad happened.");
             }
         }
-    }
-
-    private void refresh(){
-        mTextView.setText("Super Fresh");
     }
 
     @Override
@@ -224,98 +148,42 @@ public class SettingsActivity extends AppCompatActivity
             case R.id.calibrateButton:
                 calibrate();
                 break;
-            case R.id.refreshButton:
-                refresh();
-                break;
             case R.id.connectButton:
                 connectButtonPressed();
                 break;
-            /*
-            case R.id.signoutButton:
-                //mBluetoothConnection.kill();
-                //userData.save()
-                this.finish();
+            //TODO: temp buttons
+            case R.id.add_record:
+                addRecord();
                 break;
-            case R.id.frontporch_signInButton:
-                fpSignIn(new Intent(this, SignInActivity.class));
+            case R.id.delete_records:
+                deleteUserRecords();
                 break;
-            */
+            case R.id.display_records:
+                displayRecords();
+                break;
+            //TODO: End temp buttons
             default:
                 break;
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            Log.d("MainActivity", "onCreateView: Start");
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-
-            Log.d("MainActivity", "onCreateView: Done");
-            return rootView;
-        }
+    private void displayRecords(){
+        tempPm.openDB();
+        ((TextView) findViewById(R.id.textDisplay)).setText(tempPm.get(Calendar.getInstance()).toString());
+        tempPm.closeDB();
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private void deleteUserRecords(){
+        tempPm.openDB();
+        tempPm.delete(PosturizeUserInfo.getInstance().getEmail());
+        tempPm.closeDB();
+        displayRecords();
+    }
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
+    private  void addRecord() {
+        tempPm.openDB();
+        tempPm.insert((float)(Math.random() * (70 - 65) + 65));
+        ((TextView) findViewById(R.id.textDisplay)).setText(tempPm.get(Calendar.getInstance()).toString());
+        tempPm.closeDB();
     }
 }
