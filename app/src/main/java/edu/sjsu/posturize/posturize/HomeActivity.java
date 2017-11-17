b5fa66d5cce9459d422f10983dc03c840f6583ca
mattmontero <matthewrmontero@gmail.com>
2017-11-16 19:30:57 -0800

    layout name refactor

--- app/src/main/java/edu/sjsu/posturize/posturize/HomeActivity.java
+++ app/src/main/java/edu/sjsu/posturize/posturize/HomeActivity.java
@@ -52,7 +52,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
+        setContentView(R.layout.activity_home);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
