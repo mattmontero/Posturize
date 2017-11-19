f8a6d9f5677d9f9bde620f713c1353fb51dee2b0
mattmontero <matthewrmontero@gmail.com>
2017-11-18 18:41:43 -0800

    Refactor bluetooth handshakes

---
+++ app/src/main/java/edu/sjsu/posturize/posturize/bluetooth/WearableState.java
@@ -0,0 +1,58 @@
+package edu.sjsu.posturize.posturize.bluetooth;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Matt on 11/18/2017.
+ */
+
+public class WearableState extends Observable{
+
+    public final static String CALIBRATED = "CALIBRATED";
+    public final static String NOT_CALIBRATED = "NOT_CALIBRATED";
+    public final static String CONNECTED = "CONNECTED";
+    public final static String DISCONNECTED = "DISCONNECTED";
+
+    private static WearableState singletonWearableState;
+    private static String isConnected;
+    private static String isCalibrated;
+
+    private WearableState(){
+        isConnected = DISCONNECTED;
+        isCalibrated = NOT_CALIBRATED;
+    }
+
+    public static WearableState getInstance(){
+        if (singletonWearableState == null){
+            singletonWearableState = new WearableState();
+        }
+        return singletonWearableState;
+    }
+
+    public void setIsCalibrated(boolean isCalibrated) {
+        WearableState.isCalibrated = ((isCalibrated) ? CALIBRATED : NOT_CALIBRATED);
+        setChanged();
+        notifyObservers(WearableState.isCalibrated);
+    }
+
+    public void setIsConnected(boolean isConnected) {
+        WearableState.isConnected = ((isConnected) ? CONNECTED : DISCONNECTED);
+        setChanged();
+        notifyObservers(WearableState.isConnected);
+    }
+
+    public boolean isCalibrated() {
+        return isCalibrated.equals(CALIBRATED);
+    }
+
+    public boolean isConnected() {
+        return isConnected.equals(CONNECTED);
+    }
+
+
+}
